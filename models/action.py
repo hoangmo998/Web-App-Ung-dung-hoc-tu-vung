db3f6124c3de016b13477164f55096f5d22cb9c4
daituongtho <levantho2781998@gmail.com>
2020-05-22 21:35:44 +0700

    add vocabulary actions

---
+++ models/action.py
@@ -0,0 +1,8 @@
+from mongoengine import Document,StringField 
+class Actions(Document):
+    image = StringField()
+    word = StringField()
+    pronunciation = StringField()
+    mean = StringField()
+    audio_link = StringField()
+    example = StringField()
